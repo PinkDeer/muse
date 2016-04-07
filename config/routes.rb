e5151457fe7ee6d1b17e421b80b403087d17dce5
PinkDeer <bunman@mail.ru>
2016-04-08 00:21:06 +0300

    Add and setup voting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts do
-	   resources :comments
+  	member do
+  		get "like", to: "posts#upvote"
+  		get "dislike", to: "posts#downvote"
+  	end
+  	resources :comments
 	end
-
-  root "posts#index"
+  root 'posts#index'
 end
